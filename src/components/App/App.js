908f62d2da382f8ce7fc4c113f052a760d8f7cfb
mischkiey <mischkieycf@gmail.com>
2020-08-01 12:44:34 -0400

    Fixing git

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'; 
+import { Redirect, Route, Switch } from 'react-router-dom'; 
 import TokenService from '../../services/token-services';
 import Header from '../Header/Header';
 import LandingPage from '../../routes/LandingPage/LandingPage';
